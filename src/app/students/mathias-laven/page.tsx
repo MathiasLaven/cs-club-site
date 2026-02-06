ae81d5049a632e72daba0b9a632a89008915acca
Mathias Laven <mjlaven@students.unwsp.edu>
2026-02-06 14:36:10 -0600

    updates to my page (wip)

--- src/app/students/mathias-laven/page.tsx
+++ src/app/students/mathias-laven/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
         <b>Mathias Laven</b>
       </h1>
       <p className="text-center text-[30px]">
-        Computer Science student and CSC member!
+        Just some guy.
       </p>
       <div className="flex space-x-10">
         <div id="about" className="w-1000 px-8 pt-30 text-[25px]">
@@ -19,13 +19,29 @@ export default function Page() {
             <br></br>I joined UNWSP as a Computer Science major in 2023.
             <br></br>I became a member of CSC in Fall 2025.
           </p>
-          <div className="pt-6">
+          <div className="pt-6 flex mb-10">
+            <div className="flex-10"></div>
+            <div className="w-25 flex-shrink-0">
+              <a href="https://github.com/MathiasLaven/">
+                <Image
+                  src="/images/josiah/github-mark-white.png"
+                  width={100}
+                  height={100}
+                  alt="Github"
+                  title="My GitHub"
+                />
+              </a>
+            </div>
+            <div className="flex-1"></div>
+            <div className="w-25 flex-shrink-0">
               <Image
                 src="/images/josiah/github-mark-white.png"
                 width={100}
                 height={100}
                 alt="Github"
               />
+            </div>
+            <div className="flex-10"></div>
           </div>
         </div>
       </div>
